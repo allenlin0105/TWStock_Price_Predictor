47b3f8b9efaf1faea6b2245de95f28cffcfb2d26
allencclin <allencclin@kkcompany.com>
2023-01-23 19:17:54 +0800

    feat: Add twstock repo, which is a third party resource for fetching taiwan stock prices

---
+++ src/twstock/__init__.py
@@ -0,0 +1,12 @@
+"""Taiwan Stock Opendata with realtime - twstock"""
+
+# from . import stock, analytics, mock, realtime
+
+# from .analytics import BestFourPoint
+# from .codes import __update_codes, twse, tpex, codes
+# from .stock import Stock
+
+from .codes import __update_codes
+from .stock import Stock
+
+__version__ = '1.3.1'
