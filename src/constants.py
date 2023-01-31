c8b4a7ee3d34c7cc45c7d7e6a137a6798e548e5e
allenlin0105 <b08902053@csie.ntu.edu.tw>
2023-01-31 16:07:34 +0800

    feat: Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -4,5 +4,8 @@ PREDICTION_FOLDER = "prediction"
 VISUALIZE_FOLDER = "visualize"
 
 LOG_FILE = "train_log.log"
+MODEL_FILE = "model.pt"
+SCALER_FILE = "min_max_scaler.gz"
+TEST_FILE = "test.csv"
 
-TRAIN, VALID = "train", "valid"
+TRAIN, VALID, TEST = "train", "valid", "test"
